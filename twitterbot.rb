7e51d0f927bf12b85ad64edb856269c711cafd0b
sanzstv <steviesanz@comcast.net>
2017-08-01 15:55:05 -0700

    twitter bot grabbing tweets with relevant phrases or hashtags

--- twitterbot.rb
+++ twitterbot.rb
@@ -5,15 +5,16 @@ require 'Twitter'
 #related topics on the Twitter
 while true
 	begin
-		client = Twitter::REST::Client.new do |config|
-		  config.consumer_key        = ENV['TWITTER_CKEY']
-		  config.consumer_secret     = ENV['TWITTER_CSECRET']
-		  config.access_token        = ENV['TWITTER_ACCESSTOKEN']
-		  config.access_token_secret = ENV['TWITTER_AT_SECRET']
-		end
+		config ={
+		  consumer_key: ENV['TWITTER_CKEY'],
+		  consumer_secret: ENV['TWITTER_CSECRET'],
+		  access_token: ENV['TWITTER_ACCESSTOKEN'],
+		  access_token_secret: ENV['TWITTER_AT_SECRET']
+		}
 
-		rest_client = Twitter::REST::Client.new config
-		streaming_client = Twitter::Streaming::Client.new(config)
+		client = Twitter::REST::Client.new(config)
+		#streaming_client = Twitter::Streaming::Client.new(config)
+		#client.update("This is a bot that reports on true crime!")
 
 		#list of keyphrases to check for, obviously need to find way to streamline
 		keyphrases = [	
@@ -41,17 +42,33 @@ while true
 			'cold case'
 		]
 
-		streaming_client.filter(:track => keyphrases.join (',')) do |tweet|
-			#articles are best but tweets will be ok to test
-			if tweet.is_a?(Twitter::Tweet) && !retweeted
-				puts tweet.text
-				rest_client.retweet tweer
+
+		search_params = {
+			result_type: "mixed",
+			count: 3
+		}
+
+		keyphrases.each do |query|
+			client.search(query, search_params).each do |tweet|
+				#don't want to retweet multiple times
+				if tweet.retweeted? == false
+					client.retweet tweet
+				end
 			end
+			puts query
 		end
-		rescue
-		 	puts 'error'
-		 	sleep 5
-		end
+		break
+		# streaming_client.filter(:track => keyphrases.join (',')) do |tweet|
+		# 	#articles are best but tweets will be ok to test
+		# 	if tweet.is_a?(Twitter::Tweet) && !retweeted
+		# 		puts tweet.text
+		# 		rest_client.retweet tweer
+		# 	end
+		# end
+		# rescue
+		#  	puts 'error'
+		#  	sleep 5
+		# end
 
 	end
 end
